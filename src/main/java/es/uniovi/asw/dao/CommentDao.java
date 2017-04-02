bd23d8afaa67793367c320615a64e7012fb84bb5
cmabad <uo237441@uniovi.es>
2017-04-02 18:40:21 +0200

    cosas

--- src/main/java/es/uniovi/asw/dao/CommentDao.java
+++ src/main/java/es/uniovi/asw/dao/CommentDao.java
@@ -1,14 +1,11 @@
 package es.uniovi.asw.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import es.uniovi.asw.PropReader;
 import es.uniovi.asw.model.Comment;
-import es.uniovi.asw.model.Proposal;
 
 public class CommentDao {
 
@@ -35,19 +32,7 @@ public class CommentDao {
 	}
 	
 	public static int save(Comment comment) {
-		try {
-			PreparedStatement stmt = conn.prepareStatement(PropReader.get("COMMENT_INSERT"));
-
-			stmt.setInt(1, comment.getUser().getId());
-			stmt.setString(2, comment.getText());
-			stmt.setString(3, ¡¡fecha!!);
-			stmt.setString(4, proposal.getText());
-			
-			return stmt.executeUpdate();		
-
-		} catch (SQLException e) {
-			return 0;		
-		}
+		System.out.println("Comentario guardado");
+		return 1;
 	}
-
 }
